cfb78ef50b55f97cb9d085761ea9b1d5631822ae
Alexander <9032231638a@gmail.com>
2023-08-22 07:35:46 +0300

    1

--- sem1/BofW.java
+++ sem1/BofW.java
@@ -2,17 +2,9 @@ package sem1;
 
 import java.math.BigDecimal;
 
-/**
- * Класс бутилированных напитков
- */
+
 public class BofW extends Drinks {
-    /**
-     * Перегруженный конструктор класса Бутилированных напитков
-     *
-     * @param name   Наименование напитка
-     * @param price  Стоимость напитка
-     * @param volume Объем напитка
-     */
+    
     public BofW(String name, BigDecimal price, Double volume) {
         super(name, price, volume);
     }
--- sem1/VendingMachine.java
+++ sem1/VendingMachine.java
@@ -1,18 +1,8 @@
 package sem1;
 
-/**
- * Интерфейс торгового автомата
- */
 public interface VendingMachine {
-    /**
-     * Внесение продуктов в торговый автомат
-     */
+   
     void initProduct();
-
-    /**
-     * Получение продукта из торгового автомата
-     *
-     * @return строку
-     */
+    
     String getProduct();
 }
