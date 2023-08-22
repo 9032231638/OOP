229c328ccef0ad3fca6f14f340baba08cc34d787
Alexander <9032231638a@gmail.com>
2023-08-22 06:55:01 +0300

    first commit

---
+++ sem1/BofW.java
@@ -0,0 +1,19 @@
+package sem1;
+
+import java.math.BigDecimal;
+
+/**
+ * Класс бутилированных напитков
+ */
+public class BofW extends Drinkables {
+    /**
+     * Перегруженный конструктор класса Бутилированных напитков
+     *
+     * @param name   Наименование напитка
+     * @param price  Стоимость напитка
+     * @param volume Объем напитка
+     */
+    public BofW(String name, BigDecimal price, Double volume) {
+        super(name, price, volume);
+    }
+}
