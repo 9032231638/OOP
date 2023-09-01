5a17bb1dc7bac2357ee14c83ecdf57da44d3d958
Alexander <9032231638a@gmail.com>
2023-09-01 22:06:30 +0300

    11

--- sem7/MeteoStore.java
+++ sem7/MeteoStore.java
@@ -2,7 +2,7 @@ package sem7;
 
 class MeteoStore {
     // сохранение данных в базу
-    boolean save(MeteoSensor meteoSensor) {
+    boolean save(MeteoSensor meteoSensor, MeteoSensor1 meteoSensor1) {
         System.out.format("Saving data from sensor [%d] at %s%n temperature - %f ; humidity - %f ; pressure - %f \n\r",
 				meteoSensor.getId(),
 				meteoSensor.getDateTime(),
@@ -12,3 +12,4 @@ class MeteoStore {
         return true;
     }
 }
+
